2c698f56d3e90f32f922b2a814de37bd14ac40a2
Ryota123 <136326820+RyotaIzumi@users.noreply.github.com>
2026-02-24 06:13:19 +0900

    システムを移植

---
+++ IwannaMapEditer/MapEditer/AutoTile.h
@@ -0,0 +1,166 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class AutoTile
+{
+public:
+
+	AutoTile() = default;
+
+	/// @brief オートタイルを作成します。
+	/// @param image オートタイルの基本画像（1x5 タイル、または 8x6 タイル）
+	SIV3D_NODISCARD_CXX20
+		explicit AutoTile(const Image& image, const int32 id)
+	{
+		if ((image.width()) == (image.height()) && image.width() == 32)
+		{
+			*this = CreateFromOneTile(image);
+			this->tileId = id;
+			this->isSingleTile = true;
+		}
+		else
+		{
+			*this = CreateFromTiles(image);
+			this->tileId = id;
+			this->isSingleTile = false;
+		}
+
+		this->m_tileSize = 32;
+	}
+
+	/// @brief タイルのサイズ（ピクセル）を返します。
+	/// @return タイルのサイズ（ピクセル）
+	[[nodiscard]]
+	int32 getTileSize() const noexcept
+	{
+		return m_tileSize;
+	}
+
+	/// @brief オートタイルを返します。
+	/// @return オートタイル
+	[[nodiscard]]
+	TextureRegion getTile(int32 id, int32 n) const
+	{
+		Point startDrawPos{ 0, 0 };
+		return m_tileTexture(startDrawPos, m_tileSize, m_tileSize);
+	}
+
+	/// @brief オートタイルの展開図のテクスチャを返します。
+	/// @return オートタイルの展開図のテクスチャ
+	[[nodiscard]]
+	const Texture& getTileTexture() const noexcept
+	{
+		return m_tileTexture;
+	}
+
+	/// @brief タイル番号を返します。
+	/// @return タイル番号
+	const int32& getTileId() const noexcept
+	{
+		return this->tileId;
+	}
+
+private:
+
+	int32 tileId;
+	bool isSingleTile = false;
+
+	struct BaseTileIndex
+	{
+		int8 topLeft, topRight, bottomLeft, bottomRight;
+	};
+
+	[[nodiscard]]
+	static Image MakeTileImage(const Image& baseTileImage, const BaseTileIndex& index, int32 tileSize)
+	{
+		const int32 halfTileSize = (tileSize / 2);
+		Image image{ Size{ tileSize, tileSize } };
+		baseTileImage(0, (tileSize * index.topLeft), halfTileSize, halfTileSize).overwrite(image, 0, 0);
+		baseTileImage(halfTileSize, (tileSize * index.topRight), halfTileSize, halfTileSize).overwrite(image, halfTileSize, 0);
+		baseTileImage(0, (tileSize * index.bottomLeft + halfTileSize), halfTileSize, halfTileSize).overwrite(image, 0, halfTileSize);
+		baseTileImage(halfTileSize, (tileSize * index.bottomRight + halfTileSize), halfTileSize, halfTileSize).overwrite(image, halfTileSize, halfTileSize);
+		return image;
+	}
+
+	[[nodiscard]]
+	static Image MakeTiles(const Image& baseTileImage, int32 tileSize)
+	{
+		constexpr std::array<BaseTileIndex, 47> BaseTileIndices =
+		{ {
+			{ 0, 0, 0, 0 }, { 0, 2, 0, 2 },	{ 2, 2, 2, 2 },	{ 2, 0, 2, 0 }, { 0, 0, 1, 1 },	{ 0, 2, 1, 4 },	{ 2, 2, 4, 4 },	{ 2, 0, 4, 1 },
+			{ 0, 2, 1, 3 },	{ 2, 0, 3, 1 },	{ 1, 3, 1, 3 },	{ 2, 2, 3, 3 },	{ 1, 1, 1, 1 },	{ 1, 4, 1, 4 },	{ 4, 4, 4, 4 },	{ 4, 1, 4, 1 },
+			{ 1, 3, 0, 2 },	{ 3, 1, 2, 0 },	{ 3, 3, 2, 2 },	{ 3, 1, 3, 1 },	{ 1, 1, 0, 0 },	{ 1, 4, 0, 2 },	{ 4, 4, 2, 2 },	{ 4, 1, 2, 0 },
+			{ 1, 4, 1, 3 },	{ 4, 1, 3, 1 },	{ 2, 2, 4, 3 },	{ 2, 2, 3, 4 },	{ 4, 4, 4, 3 },	{ 4, 4, 3, 4 },	{ 4, 3, 3, 3 },	{ 3, 4, 3, 3 },
+			{ 1, 3, 1, 4 },	{ 3, 1, 4, 1 }, { 4, 3, 2, 2 },	{ 3, 4, 2, 2 },	{ 4, 3, 4, 4 },	{ 3, 4, 4, 4 },	{ 3, 3, 4, 3 },	{ 3, 3, 3, 4 },
+			{ 3, 3, 4, 4 },	{ 4, 4, 3, 3 },	{ 4, 3, 4, 3 },	{ 3, 4, 3, 4 },	{ 4, 3, 3, 4 },	{ 3, 4, 4, 3 },	{ 3, 3, 3, 3 }
+		} };
+
+		Image image{ Size{ (tileSize * 8), (tileSize * 6) }, Color{ 255, 0 } };
+
+		for (int32 i = 0; i < 47; ++i)
+		{
+			const int32 x = ((i % 8) * tileSize);
+			const int32 y = ((i / 8) * tileSize);
+			MakeTileImage(baseTileImage, BaseTileIndices[i], tileSize).overwrite(image, x, y);
+		}
+
+		return image;
+	}
+
+	[[nodiscard]]
+	static uint8 GetTileIndex(uint8 bits) noexcept
+	{
+		constexpr std::array<uint8, 256> Indices =
+		{
+			 0,  0,  4,  4,  0,  0,  4,  4,  1,  1,  8,  5,  1,  1,  8,  5,
+			 3,  3,  9,  9,  3,  3,  7,  7,  2,  2, 11, 27,  2,  2, 26,  6,
+			 0,  0,  4,  4,  0,  0,  4,  4,  1,  1,  8,  5,  1,  1,  8,  5,
+			 3,  3,  9,  9,  3,  3,  7,  7,  2,  2, 11, 27,  2,  2, 26,  6,
+			20, 20, 12, 12, 20, 20, 12, 12, 16, 16, 10, 32, 16, 16, 10, 32,
+			17, 17, 19, 19, 17, 17, 33, 33, 18, 18, 46, 39, 18, 18, 38, 40,
+			20, 20, 12, 12, 20, 20, 12, 12, 21, 21, 24, 13, 21, 21, 24, 13,
+			17, 17, 19, 19, 17, 17, 33, 33, 35, 35, 31, 43, 35, 35, 45, 37,
+			 0,  0,  4,  4,  0,  0,  4,  4,  1,  1,  8,  5,  1,  1,  8,  5,
+			 3,  3,  9,  9,  3,  3,  7,  7,  2,  2, 11, 27,  2,  2, 26,  6,
+			 0,  0,  4,  4,  0,  0,  4,  4,  1,  1,  8,  5,  1,  1,  8,  5,
+			 3,  3,  9,  9,  3,  3,  7,  7,  2,  2, 11, 27,  2,  2, 26,  6,
+			20, 20, 12, 12, 20, 20, 12, 12, 16, 16, 10, 32, 16, 16, 10, 32,
+			23, 23, 25, 25, 23, 23, 15, 15, 34, 34, 30, 44, 34, 34, 42, 36,
+			20, 20, 12, 12, 20, 20, 12, 12, 21, 21, 24, 13, 21, 21, 24, 13,
+			23, 23, 25, 25, 23, 23, 15, 15, 22, 22, 41, 29, 22, 22, 28, 14
+		};
+
+		return Indices[bits];
+	}
+
+	[[nodiscard]]
+	static AutoTile CreateFromBaseImage(const Image& baseTileImage_1x5)
+	{
+		AutoTile autoTiles;
+		autoTiles.m_tileSize = baseTileImage_1x5.width();
+		autoTiles.m_tileTexture = Texture{ MakeTiles(baseTileImage_1x5, autoTiles.m_tileSize) };
+		return autoTiles;
+	}
+
+	[[nodiscard]]
+	static AutoTile CreateFromTiles(const Image& tiles)
+	{
+		AutoTile autoTiles;
+		autoTiles.m_tileSize = 32;
+		autoTiles.m_tileTexture = Texture{ tiles };
+		return autoTiles;
+	}
+
+	[[nodiscard]]
+	static AutoTile CreateFromOneTile(const Image& tile)
+	{
+		AutoTile autoTile;
+		autoTile.m_tileSize = (tile.width());
+		autoTile.m_tileTexture = Texture{ tile };
+		return autoTile;
+	}
+
+	int32 m_tileSize = 1;
+
+	Texture m_tileTexture;
+};
---
+++ IwannaMapEditer/MapEditer/Main.cpp
@@ -0,0 +1,679 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.16
+#include "AutoTile.h"
+
+/// @brief オートタイルの接続情報
+struct AutoTileConnectivity
+{
+	bool connected[8] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+};
+
+/// @brief オブジェクト上でカーソルを描きます。(タイルモード時)
+void DrawTileCursor()
+{
+	Cursor::RequestStyle(CursorStyle::Hidden);
+	const Vec2 cursorPos = Cursor::PosF();
+	const Triangle triangle{ cursorPos, (cursorPos + Vec2{ 20, 6 }), (cursorPos + Vec2{ 6, 20 }) };
+	triangle.stretched(1.2).movedBy(0.0, 0.5).draw(ColorF{ 0.25 });
+	triangle.draw();
+}
+
+/// @brief オブジェクト上でカーソルを描きます。(ギミックモード時)
+void DrawGimmikCursor(Vec2 pos, Texture gimmikTexture)
+{
+	gimmikTexture(0,0,32,32).drawAt(pos.x + 16,pos.y + 16);
+}
+
+/// @brief マウスカーソルがあるタイルのインデックスを返します。
+/// @param size オブジェクト数
+/// @param tileSize タイルのサイズ（ピクセル）
+/// @param offset タイルの描画位置のオフセット
+/// @return タイルのインデックス。オブジェクト上にカーソルがない場合は none
+[[nodiscard]]
+Optional<Point> GetCursorIndex(const Size& size, int32 tileSize, const Point& offset)
+{
+	const Point cursorPos = (Cursor::Pos() - offset);
+
+	if ((not InRange(cursorPos.x, 0, (size.x * tileSize - 1)))
+		|| (not InRange(cursorPos.y, 0, (size.y * tileSize - 1))))
+	{
+		return none;
+	}
+
+	return{ cursorPos / tileSize };
+}
+
+int32 GetAutoTileIndexById(const Array<AutoTile> tiles, int32 id) {
+	for (size_t i = 0; i < tiles.size(); i++) {
+		if (tiles[i].getTileId() == id) {
+			return i;
+		}
+	}
+	return 0;
+}
+
+int32 GetSurroundingTileId(const Grid<uint32>& grid, Size size, int32 x, int32 y, int32 direction) {
+	int32 id = 0;
+
+	switch (direction) {
+	case 0: // 上
+		if (y - 1 < 0) break;
+		id = grid[y - 1][x];
+		break;
+	case 1: // 下
+		if (y + 1 > size.y - 1) break;
+		id = grid[y + 1][x];
+		break;
+	case 2: // 左
+		if (x - 1 < 0) break;
+		id = grid[y][x - 1];
+		break;
+	case 3: // 右
+		if (x + 1 > size.x - 1) break;
+		id = grid[y][x + 1];
+		break;
+	default:
+		return 0;
+		break;
+	}
+
+	return id;
+}
+
+// マップサイズ（GUIで変更可能）
+Size mapSize(25, 19);
+TextEditState mapWidthText;
+TextEditState mapHeightText;
+Size gridSize(mapSize);
+
+// マップデータ（動的に生成される）
+Grid<uint32> grid(gridSize, 0);
+Grid<AutoTileConnectivity> connectivityGrid(gridSize);
+
+//保存ファイル名
+TextEditState saveFileName;
+
+
+void Main()
+{
+	Window::SetTitle(U"I wanna Map Editor");
+	Window::Resize(1280, 720);
+	Scene::SetBackground(ColorF{ 0.75 });
+
+	// オブジェクト画像の読み込み
+	const FilePath objectPath = U"texture/object/";
+	const Texture baseTexture{ objectPath + U"none.png" };
+	const Array<AutoTile> autoTiles =
+	{
+		AutoTile{ Image{ objectPath + U"sprBlock.png" }, 1 },
+		AutoTile{ Image{ objectPath + U"sprFloor.png" }, 2 },
+		AutoTile{ Image{ objectPath + U"sprFloor2.png" }, 3 },
+		AutoTile{ Image{ objectPath + U"sprFloor3.png" }, 4 },
+		AutoTile{ Image{ objectPath + U"sprFloor4.png" }, 5 },
+		AutoTile{ Image{ objectPath + U"sprFloor5.png" }, 6 },
+		AutoTile{ Image{ objectPath + U"sprFloor6.png" }, 7 },
+		AutoTile{ Image{ objectPath + U"sprSpikeUp.png" }, 21 },
+		AutoTile{ Image{ objectPath + U"sprSpikeLeft.png" }, 22 },
+		AutoTile{ Image{ objectPath + U"sprSpikeDown.png" }, 23 },
+		AutoTile{ Image{ objectPath + U"sprSpikeRight.png" }, 24 },
+		AutoTile{ Image{ objectPath + U"sprSave.png" }, 25 },
+	};
+	size_t autoTileIndex = 0;
+
+	// 敵の名前配列（ListBox と一致させる）
+	const Array<String> gimmikNames = { U"罠針_上", U"罠針_左", U"罠針_下",U"罠針_右", U"罠トリガー"};
+
+	//敵の準備
+	ListBoxState listBoxGimmiks{
+		{
+			gimmikNames
+		}
+	};
+	listBoxGimmiks.selectedItemIndex = 0;
+
+	ListBoxState listBoxPlacedGimmiks;
+	listBoxPlacedGimmiks.selectedItemIndex = 0;
+
+	const FilePath gimmikPath = U"texture/enemy/";
+	const Array<Texture> gimmikTextures =
+	{
+		Texture{ gimmikPath + U"spikeTrapUp.png" },
+		Texture{ gimmikPath + U"spikeTrapLeft.png" },
+		Texture{ gimmikPath + U"spikeTrapDown.png" },
+		Texture{ gimmikPath + U"spikeTrapRight.png" },
+		Texture{ gimmikPath + U"trapTrigger.png" },
+	};
+
+	// --- 敵情報構造体 ---
+	struct GimmikInfo
+	{
+		String name;
+		Vec2 pos;      // マップ上のピクセル座標（左上原点）
+		int32 trapId;  // トリガーと結び付けるID
+		double direction; // 罠の向き
+		double speed; // 罠の速度
+	};
+
+	// 配置済みの敵リスト
+	Array<GimmikInfo> placedGimmiks;
+
+	// プレイヤー開始位置（固定にしてるが GUI で変更したければ TextEditState 等にする）
+	Vec2 startPlayerPos = Vec2{ 30.0, 30.0 };
+
+	// テキストボックスの初期文字列
+	String posXText = Format(startPlayerPos.x);
+	String posYText = Format(startPlayerPos.y);
+
+	// テキストボックス
+	TextEditState editX;
+	TextEditState editY;
+	editX.text = posXText;
+	editY.text = posYText;
+
+	// 敵の値（GUI で変更可能にするためのテキスト入力）
+	TextEditState gimmikIdText;
+	TextEditState gimmikDirText;
+	TextEditState gimmikSpdText;
+
+
+
+	// マップのセルの数
+	 Size GridSize{ 25, 19 };
+
+	// 選択オブジェクト欄のサイズ
+	constexpr Size TileSize{ 5,9 };
+
+	// マップを描画するときのオフセット
+	constexpr Point LayerOffset{ 40, 40 };
+
+	// オブジェクト選択欄のオフセット
+	constexpr Point TileSelectOffset{ 900, 140 };
+
+	// オートタイルの有無を格納する二次元配列
+	Grid<uint32> grid(GridSize, 0);
+
+	Grid<uint32> tileGrid(TileSize, 0);
+
+	// オートタイルの接続情報を格納する二次元配列
+	Grid<AutoTileConnectivity> connectivityGrid(GridSize);
+
+	// 選択されているタイルのインデックス(左)
+	Optional<Point> selectedTileIndex;
+	// 選択されているタイルのインデックス(右)
+	Optional<Point> selectedTileCursorIndex = Point{0,0};
+	// 選択されているオブジェクトID
+	int32 selectedTileId = 1;
+	// 選択されているオブジェクトIDがautoTilesの何番目か
+	int32 selectedTileNumberInAutoTiles = 0;
+
+	// マップサイズ入力欄の初期テキスト
+	mapWidthText.text = Format(mapSize.x);
+	mapHeightText.text = Format(mapSize.y);
+
+	// 横スクロール位置（0 ～ mapSize.x-25）
+	double scrollX = 0;
+	double scrollY = 0;
+
+	// 設置物選択GUI用変数
+	size_t settingMode = 0;//0: tile, 1: gimmik
+
+	// 敵の値初期文字列
+	gimmikIdText.text = U"0";
+	gimmikDirText.text = U"0";
+	gimmikSpdText.text = U"0";
+
+	while (System::Update())
+	{
+		if (KeyC.down()) {
+			ClearPrint();
+		}
+
+		//フォント宣言
+		FontAsset::Register(U"Font", 20, Typeface::Regular);
+
+		// 背景の市松模様を描く
+		for (int32 y = 0; y < (Scene::Height() / 20); ++y)
+		{
+			for (int32 x = 0; x < (Scene::Width() / 20); ++x)
+			{
+				if (IsEven(y + x))
+				{
+					Rect{ (x * 20), (y * 20), 20 }.draw(ColorF{ 0.7 });
+				}
+			}
+		}
+
+		Rect(870, 0, 800, 800).draw(ColorF{ 0.1,0.5,0.1, 1.0 });
+
+
+		// === 設置物選択GUI ===
+		SimpleGUI::RadioButtons(settingMode, { U"ブロック", U"ギミック" }, Vec2{ 900, 40 });
+
+		// === マップサイズ入力GUI ===
+		FontAsset(U"Font")(U"マップの大きさ\nwidth        height").draw(1080, 10);
+		SimpleGUI::TextBox(mapWidthText, Vec2{ 1080, 70}, 80);
+		SimpleGUI::TextBox(mapHeightText, Vec2{ 1180,70}, 80);
+
+		// 安全に文字列を int32 に変換して Clamp を適用
+		if (const auto optW = s3d::ParseIntOpt<int32>(mapWidthText.text))
+		{
+			mapSize.x = Clamp(*optW, 1, 200);
+		}
+		// 同様に高さ
+		if (const auto optH = s3d::ParseIntOpt<int32>(mapHeightText.text))
+		{
+			mapSize.y = Clamp(*optH, 1, 200);
+		}
+
+		// サイズが変更されたらグリッドを再生成
+		if (gridSize.x != mapSize.x || gridSize.y != mapSize.y)
+		{
+			gridSize = Size{ mapSize.x, mapSize.y };
+			grid = Grid<uint32>(gridSize, 0);
+			connectivityGrid = Grid<AutoTileConnectivity>(gridSize);
+		}  
+        
+		// === 横スクロールバー ===
+
+		const int viewWidth = 25; // 表示するオブジェクト数（固定）
+		const int viewHeight = 19;
+
+		// 描画開始 X（スクロール反映）
+		int startX = scrollX;
+		int startY = scrollY;
+		int endX = Min((int)scrollX + viewWidth, mapSize.x);
+		int endY = Min((int)scrollY + viewHeight, mapSize.y);
+		
+		// mapSize.x が 25 以上の場合のみ表示
+		if (mapSize.x > viewWidth)
+		{
+			SimpleGUI::Slider(scrollX, 0, (mapSize.x - viewWidth), Vec2{ 40, 660 }, 800, 120);
+		}
+		else
+		{
+			scrollX = 0; // 小さい場合はスクロール禁止
+		}
+
+		// mapSize.y が 19 以上の場合のみ表示
+		if (mapSize.y > viewHeight)
+		{
+			SimpleGUI::VerticalSlider(scrollY, 0, (mapSize.y - viewHeight), Vec2{ 0, 40 }, 608, 120);
+		}
+		else
+		{
+			scrollY = 0; // 小さい場合はスクロール禁止
+		}
+
+		// === マップ保存ボタン & ファイル名指定text box ===
+		FontAsset(U"Font")(U"保存ファイル名").draw(1080, 120);
+		SimpleGUI::TextBox(saveFileName, Vec2{ 1080, 150 }, 150);
+		if (SimpleGUI::Button(U"Save CSV", Vec2{ 1080, 200 }))
+		{
+			CSV csv;
+			const FilePath path = U"./MapData/" + saveFileName.text + U".csv";
+			if (!path.isEmpty())
+			{
+				for (int y = 0; y < grid.height(); ++y)
+				{
+					for (int x = 0; x < grid.width(); ++x)
+					{
+						csv.write(grid[y][x]);
+					}
+					csv.newLine();
+				}
+				csv.save(path);
+				Print << U"保存しました: " << path;
+			}
+		}
+
+		if (SimpleGUI::Button(U"Save JSON", Vec2{ 1080, 240 }))
+		{
+			// JSON を手作りで組み立てる（シンプル）
+			// 望ましい形式:
+			// [
+			//  {
+			//    "startPlayerPos": [30, 30],
+			//    "Gimmiks": [
+			//      { "gimmikName": "crab", "gimmikPos": [1500, 510], "gimmikValue": 1.8 },
+			//      ...
+			//    ]
+			//  }
+			// ]
+			// 
+			// トップレベル配列 JSON を作る
+			JSON json;
+			//json.setArray();  // これが最重要！
+
+			JSON root;  // 配列に入れるオブジェクト
+
+			const FilePath path = U"./MapData/" + saveFileName.text + U".json";
+
+			// --- startPlayerPos ---
+			root[U"startPlayerPos"].push_back(startPlayerPos.x);
+			root[U"startPlayerPos"].push_back(startPlayerPos.y);
+
+			// --- Gimmiks ---
+			for (const auto& e : placedGimmiks)
+			{
+				JSON gimmik;
+
+				gimmik[U"gimmikName"] = e.name;
+
+				gimmik[U"gimmikPos"].push_back(e.pos.x);
+				gimmik[U"gimmikPos"].push_back(e.pos.y);
+
+				gimmik[U"id"] = e.trapId;
+				gimmik[U"direction"] = e.direction;
+				gimmik[U"speed"] = e.speed;
+
+				root[U"Gimmiks"].push_back(gimmik);
+			}
+
+			// root を配列に追加
+			json.push_back(root);
+
+			// 保存
+			json.save(path);
+			Print << U"情報を保存しました: " << path;
+		}
+
+
+
+
+
+		// 現在のオートタイルの種類
+		const auto& autoTile = autoTiles[autoTileIndex];
+		const int32 tileSize = autoTile.getTileSize();
+
+		// カーソルでマウスオーバーしているタイルのインデックス
+		auto cursorIndex = GetCursorIndex(Size{ viewWidth, mapSize.y }, tileSize, LayerOffset);
+
+		if (cursorIndex)
+		{
+			// 実際のマップ座標へ補正
+			cursorIndex->x += scrollX;
+			cursorIndex->y += scrollY;
+
+			// 範囲外なら無効化
+			if (cursorIndex->x >= mapSize.x || cursorIndex->y >= mapSize.y)
+				cursorIndex = none;
+		}
+
+		const auto touchedTilCcursorIndex = GetCursorIndex(tileGrid.size(), tileSize, TileSelectOffset);
+
+		// クリック時の配置処理
+		if (settingMode == 0) {
+
+			// クリックでタイルを編集する
+			if (cursorIndex && (MouseL.pressed() || MouseR.pressed()))
+			{
+				if (MouseL.pressed())
+				{
+					grid[*cursorIndex] = selectedTileId;
+				}
+				else
+				{
+					grid[*cursorIndex] = 0;
+					connectivityGrid[*cursorIndex] = AutoTileConnectivity{};
+				}
+
+				selectedTileIndex = *cursorIndex;
+			}
+		} // --- 敵配置モードで左クリックしたら敵を配置 ---
+		else if (settingMode == 1 && MouseL.down()) {
+				// カーソルがマップ描画領域上にあるかを判定（GetCursorIndex を再利用）
+				// viewWidth と startX は既に計算済みの前提
+				const int viewWidth = 25; // 既定値と同じにしておく
+				const auto maybeIndex = GetCursorIndex(Size{ viewWidth, mapSize.y }, tileSize, LayerOffset);
+				if (maybeIndex){
+					// world（マップ全体に対するピクセル座標）を計算する
+					// 描画は ( (tileX - startX) * tileSize + LayerOffset.x ) なので逆算する
+					const Vec2 cursorF = Cursor::PosF();
+					// startX はスクロールから計算している描画開始オブジェクト
+					// worldX = (cursorF.x - LayerOffset.x) + startX * tileSize
+					Point worldPos{
+						(cursorIndex->x - startX) * tileSize,
+						(cursorIndex->y - startY) * tileSize
+					};
+					
+					// 罠の値をパース（失敗時は 0.0 をデフォルトに）
+					double val = 0.0;
+					if (const auto d = Parse<int32>(gimmikIdText.text)) val = d;
+
+					GimmikInfo e;
+					e.name = gimmikNames[*listBoxGimmiks.selectedItemIndex]; // listBox の選択と同期
+					e.pos = Vec2{ worldPos };
+					e.trapId = val;
+					e.direction = val;
+					e.speed = val;
+
+					placedGimmiks.push_back(e);
+					listBoxPlacedGimmiks.items.push_back(e.name);
+					Print << U"敵を追加: " << e.name << U" at " << e.pos << U" value=" << val;
+				}
+			}
+
+
+		// マップチップを描く
+		for (int32 y = 0; y < grid.height(); ++y)
+		{
+			for (int32 x = 0; x < grid.width(); ++x)
+			{
+				// 0〜24 の表示範囲から外れたら描画しない
+				if (not InRange(x, startX, endX - 1) || not InRange(y, startY, endY - 1))
+				{
+					continue;
+				}
+
+				// 画面上の描画位置（x をスクロール補正）
+				Point drawPos = Point{ (x - startX), y - startY } *tileSize + LayerOffset;
+				//入力が無い場合、透明タイルを描画する
+				baseTexture.draw(drawPos);
+
+				if (grid[y][x]) {
+					//描画対象のタイルを取得する
+					const auto targetTile = autoTiles[GetAutoTileIndexById(autoTiles, grid[y][x])];
+					int32 targetId = targetTile.getTileId();
+
+					targetTile.getTile(targetId,0).draw(drawPos, settingMode == 0 ? ColorF{ 1.0, 1.0 } : ColorF{ 1.0, 0.5 });
+				}
+				else
+				{
+					Rect{ drawPos, tileSize }.drawFrame(1, ColorF{ 0.5 });
+				}
+			}
+		}
+
+		int placedgimmikCount = 0;
+		// placedGimmiks にある敵の描画
+		for (const auto& e : placedGimmiks){
+			// e.name に対応する gimmikNames のインデックスを探す（存在しなければ -1）
+			int idx = -1;
+			for (int i = 0; i < static_cast<int>(gimmikNames.size()); ++i)
+			{
+				if (gimmikNames[i] == e.name)
+				{
+					idx = i;
+					break;
+				}
+			}
+
+			if (0 <= idx && idx < static_cast<int>(gimmikTextures.size())){
+				Vec2 drawgimmikPos = Vec2{
+					(e.pos.x - (scrollX * tileSize)) + LayerOffset.x,
+					(e.pos.y - (scrollY * tileSize)) + LayerOffset.y
+				};
+				if (0 <= drawgimmikPos.x && drawgimmikPos.x < 800) {
+					gimmikTextures[idx](0, 0, 32, 32).drawAt(drawgimmikPos.x + 16, drawgimmikPos.y + 16).drawFrame(
+						1.0, placedgimmikCount == listBoxPlacedGimmiks.selectedItemIndex ? ColorF(1.0, 0.0, 0.0, 1.0) : ColorF(0.0, 0.0));
+				}
+			}
+			else{
+				// 名前が一致しないなら簡易に円で表す
+				Circle{ e.pos, 8 }.draw(ColorF{ 1.0, 0.2 });
+			}
+			placedgimmikCount++;
+		}
+
+
+		// カーソルが重なっているタイルを強調表示する
+		// マップタイル強調表示（scrollX 補正済）
+		if (cursorIndex)
+		{
+			// 表示用の X 座標は (cursorIndex->x - startX)
+			Point highlightPos{
+				(cursorIndex->x - startX) * tileSize,
+				(cursorIndex->y - startY) * tileSize
+			};
+			highlightPos += LayerOffset;
+
+			switch (settingMode) {
+				case 0: // オブジェクトモードのときはオレンジで強調
+					Rect{ highlightPos, tileSize }.draw(ColorF{ 1.0, 0.5, 0.0, 0.5 });
+					DrawTileCursor();
+					break;
+				case 1: // ギミックモードのときはそれ自体を描画
+					DrawGimmikCursor(highlightPos, gimmikTextures[*listBoxGimmiks.selectedItemIndex]);
+					break;
+			}
+		}
+
+		int32 autoTilesValue = 0;
+		int32 tileCounter = 1;
+
+		// 設置物選択GUIの描画
+
+		// 座標の固定値
+		const Vec2 baseJsonValueUIPos = { 1100,580 };
+
+		if (settingMode == 0) {// オブジェクト設置モードのとき
+			// オブジェクト選択欄を描画する
+			for (int32 y = 0; y < tileGrid.height(); ++y) {
+				for (int32 x = 0; x < tileGrid.width(); ++x) {
+					if (autoTiles[autoTilesValue].getTileId() == tileCounter) {
+						autoTiles[autoTilesValue].getTile(tileCounter, 0).draw(Point{ x, y } *tileSize + TileSelectOffset);
+						if (autoTilesValue != autoTiles.size() - 1) autoTilesValue++;
+					}
+					Rect{ (Point{ x, y } *tileSize + TileSelectOffset), tileSize }.drawFrame(1, ColorF{ 1.0 });
+					tileCounter++;
+				}
+			}
+
+			// オブジェクト選択欄のカーソルが重なっているタイルを強調表示する
+			if (touchedTilCcursorIndex)
+			{
+				Rect{ (*touchedTilCcursorIndex * tileSize + TileSelectOffset), tileSize }.draw(ColorF{ 1.0, 0.5, 0.0, 0.0 }).drawFrame(2, Palette::Blue);
+				DrawTileCursor();
+			}
+
+			// オブジェクト選択欄でクリックされたら、そのタイルを選択する
+			if (touchedTilCcursorIndex && MouseL.down())
+			{
+				selectedTileCursorIndex = touchedTilCcursorIndex;
+				selectedTileId = (touchedTilCcursorIndex->y * tileGrid.width() + touchedTilCcursorIndex->x + 1);
+
+				bool isFound = false;// autoTiles内で選択されたオブジェクトIDが存在するかを確認
+				for (size_t i = 0; i < autoTiles.size(); i++) {
+					if (autoTiles[i].getTileId() == selectedTileId) {
+						isFound = true;
+					}
+				}
+
+				if (!isFound) selectedTileId = 0;//存在しない場合は透明タイルを選択
+			}
+
+			// 選択されているタイルを赤で強調表示する
+			Rect{ (*selectedTileCursorIndex * tileSize + TileSelectOffset), tileSize }.draw(ColorF{ 1.0, 0.5, 0.0, 0.0 }).drawFrame(2, Palette::Red);
+		}
+		else {// 敵設置モードのとき
+			SimpleGUI::ListBox(listBoxGimmiks, Vec2{ 900, 140 }, 120, 156);
+			SimpleGUI::ListBox(listBoxPlacedGimmiks, Vec2{ 900, 350 }, 120, 300);
+
+			if (listBoxPlacedGimmiks.selectedItemIndex) {
+				const size_t idx = static_cast<size_t>(*listBoxPlacedGimmiks.selectedItemIndex);
+
+				if (SimpleGUI::Button(U"Delete", Vec2{ baseJsonValueUIPos.x, 350 })) {
+					placedGimmiks.remove_at(idx);
+					listBoxPlacedGimmiks.items.remove_at(idx);
+				}
+
+				// === id入力GUI ===
+				FontAsset(U"Font")(U"id : ").draw(baseJsonValueUIPos.x, 390);
+				FontAsset(U"Font")(U"角度 : ").draw(baseJsonValueUIPos.x, 430);
+				FontAsset(U"Font")(U"速度 : ").draw(baseJsonValueUIPos.x, 470);
+
+				static Optional<size_t> prevIdx = none;
+
+				// idx が変わったときだけ text を初期化する
+				if (prevIdx != idx)
+				{
+					gimmikIdText.text = Format(placedGimmiks[idx].trapId);
+					gimmikDirText.text = Format(placedGimmiks[idx].direction);
+					gimmikSpdText.text = Format(placedGimmiks[idx].speed);
+					prevIdx = idx;
+				}
+
+				SimpleGUI::TextBox(gimmikIdText, Vec2{ baseJsonValueUIPos.x + 70, 390 }, 80);
+				SimpleGUI::TextBox(gimmikDirText, Vec2{ baseJsonValueUIPos.x + 70, 430 }, 80);
+				SimpleGUI::TextBox(gimmikSpdText, Vec2{ baseJsonValueUIPos.x + 70, 470 }, 80);
+
+				// 変更があった瞬間だけ value に反映
+				if (gimmikIdText.textChanged && gimmikIdText.text != U"")
+				{
+					if (const auto evt = Parse<double>(gimmikIdText.text))
+					{
+						// clamp range 例：0.0〜999.0 とか
+						placedGimmiks[idx].trapId = Math::Max(0.0, evt);
+					}
+				}
+
+				// 変更があった瞬間だけ value に反映
+				if (gimmikDirText.textChanged && gimmikDirText.text != U"")
+				{
+					if (const auto evt = Parse<double>(gimmikDirText.text))
+					{
+						// clamp range 例：0.0〜999.0 とか
+						placedGimmiks[idx].direction = Math::Max(0.0, evt);
+					}
+				}
+
+				// 変更があった瞬間だけ value に反映
+				if (gimmikSpdText.textChanged && gimmikSpdText.text != U"")
+				{
+					if (const auto evt = Parse<double>(gimmikSpdText.text))
+					{
+						// clamp range 例：0.0〜999.0 とか
+						placedGimmiks[idx].speed = Math::Max(0.0, evt);
+					}
+				}
+			}
+
+			// --- UI描画 ---
+
+			FontAsset(U"Font")(U"Player初期座標").draw(baseJsonValueUIPos.x, baseJsonValueUIPos.y, Palette::White);
+			FontAsset(U"Font")(U"x").draw(baseJsonValueUIPos.x, baseJsonValueUIPos.y + 40, Palette::White);
+			FontAsset(U"Font")(U"y").draw(baseJsonValueUIPos.x, baseJsonValueUIPos.y + 80, Palette::White);
+
+			// X テキストボックス
+			if (SimpleGUI::TextBox(editX, Vec2{ baseJsonValueUIPos.x + 20, baseJsonValueUIPos.y + 40 }, 80))
+			{
+				// 入力されたとき、数値か判定
+				if (editX.text)
+				{
+					startPlayerPos.x = Parse<int>(editX.text);
+				}
+			}
+
+			// Y テキストボックス
+			if (SimpleGUI::TextBox(editY, Vec2{ baseJsonValueUIPos.x + 20, baseJsonValueUIPos.y + 80 }, 80))
+			{
+				if (editY.text)
+				{
+					startPlayerPos.y = Parse<int>(editY.text);
+				}
+			}
+
+			// プレイヤー開始位置を描画する
+			Circle{ startPlayerPos - Vec2{ scrollX * tileSize, scrollY * tileSize } + Vec2{ LayerOffset.x, LayerOffset.y }, 10 }.draw(Palette::Yellow);
+		}
+	}
+}
