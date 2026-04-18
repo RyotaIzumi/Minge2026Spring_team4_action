541c9dba1934daa643362cb092220d29a6debab2
Ryota123 <136326820+RyotaIzumi@users.noreply.github.com>
2026-04-18 12:29:22 +0900

    jsonのロード、出力機能追加

---
+++ I wanna Siv3D/OpenSiv3D_0.6.161/MainGameSerializer.cpp
@@ -0,0 +1,36 @@
+﻿#include "MainGameSerializer.h"
+
+MainGameSerializer::MainGameSerializer() {
+}
+
+MainGameSerializer::~MainGameSerializer() {
+}
+
+void MainGameSerializer::LoadCharactersMoraleValue() {
+	const JSON json = JSON::Load(U"CharactersMoraleValue.json");
+
+	if (!json) {
+		throw Error{ U"キャラクター士気度JSONファイルをロードできませんでした" };
+	}
+
+	moraleValue1 = json[U"MoraleValue1"].get<int32>();
+	moraleValue2 = json[U"MoraleValue2"].get<int32>();
+	moraleValue3 = json[U"MoraleValue3"].get<int32>();
+	moraleValue4 = json[U"MoraleValue4"].get<int32>();
+}
+
+void MainGameSerializer::defineGlobalStatuses() {
+	if (moraleValue2 > 90)Global::startRoomName = U"trap1";
+	else Global::startRoomName = U"normal2";
+}
+
+// エンディング種類値を保存する処理をここに実装
+void MainGameSerializer::SaveEndingValue() {
+	JSON json;
+
+	// 値をセット
+	json[U"EndingValue"] = 0;
+
+	// ファイル保存
+	json.save(U"EndingValue.json");
+}
